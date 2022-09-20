4bf784641c02b6af9af390a2ea90ab2f64bd687b
michelle onasanya <michelle.onasanya@amaro.com>
2022-09-20 15:05:12 -0300

    Início da criação da tela de detalhe

--- Marvel/View/Details/DetailViewCode.swift
+++ Marvel/View/Details/DetailViewCode.swift
@@ -12,6 +12,30 @@ class DetailsViewCode: UIView {
     
     //MARK: - Atributos
     let backgroundDetails = UIView(frame: UIScreen.main.bounds)
+    let imageChatacters = UIImageView()
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configHierarchy()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configHierarchy() {
+        
+    }
+    
+    func configConstraints() {
+        
+    }
+    
     
     
 }
