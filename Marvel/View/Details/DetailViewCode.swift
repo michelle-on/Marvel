477a40a751b78129641d8d33dd8ece099108529e
michelle onasanya <michelle.onasanya@amaro.com>
2022-09-23 09:23:02 -0300

    Implementation and update of the character details screen

--- Marvel/View/Details/DetailViewCode.swift
+++ Marvel/View/Details/DetailViewCode.swift
@@ -11,7 +11,7 @@ import UIKit
 class DetailsViewCode: UIView {
     
     //MARK: - Atributos
-    let backgroundDetails = UIView(frame: UIScreen.main.bounds)
+    let backgroundDetails = UIView()
     let imageChatacters = UIImageView()
     let nameLabel = UILabel()
     let descriptionLabel = UILabel()
@@ -29,13 +29,82 @@ class DetailsViewCode: UIView {
     }
     
     func configHierarchy() {
+        self.addSubview(backgroundDetails)
         
+        self.backgroundDetails.addSubview(imageChatacters)
+        self.backgroundDetails.addSubview(nameLabel)
+        self.backgroundDetails.addSubview(descriptionLabel)
+        
+        self.imageChatacters.backgroundColor = UIColor.lightGray
+        self.backgroundDetails.backgroundColor = UIColor.white
+                
+        self.backgroundDetails.translatesAutoresizingMaskIntoConstraints = false
+        self.imageChatacters.translatesAutoresizingMaskIntoConstraints = false
+        self.nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nameLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        self.nameLabel.textColor = UIColor.black
+        
+        self.descriptionLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        self.descriptionLabel.numberOfLines = 0
     }
     
     func configConstraints() {
+        NSLayoutConstraint.activate([
         
+            self.backgroundDetails.topAnchor.constraint(equalTo: self.topAnchor),
+            self.backgroundDetails.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.backgroundDetails.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.backgroundDetails.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
+            self.imageChatacters.topAnchor.constraint(equalTo: self.backgroundDetails.topAnchor),
+            self.imageChatacters.leadingAnchor.constraint(equalTo: self.backgroundDetails.leadingAnchor),
+            self.imageChatacters.trailingAnchor.constraint(equalTo: self.backgroundDetails.trailingAnchor),
+            self.imageChatacters.heightAnchor.constraint(equalToConstant: 300),
+            
+            self.nameLabel.topAnchor.constraint(equalTo: self.imageChatacters.bottomAnchor, constant: 5),
+            self.nameLabel.leadingAnchor.constraint(equalTo: self.backgroundDetails.leadingAnchor, constant: 5),
+            self.nameLabel.trailingAnchor.constraint(equalTo: self.backgroundDetails.trailingAnchor),
+            self.nameLabel.bottomAnchor.constraint(equalTo: self.descriptionLabel.topAnchor, constant: -15),
+            
+            self.descriptionLabel.leadingAnchor.constraint(equalTo: self.backgroundDetails.leadingAnchor, constant: 5),
+            self.descriptionLabel.trailingAnchor.constraint(equalTo: self.backgroundDetails.trailingAnchor, constant: -5)
+        ])
     }
     
+    func configDetails (_ character: Character) {
+        nameLabel.text = character.name
+        descriptionLabel.text = character.description
+        
+        guard let `extension` = character.thumbnail?.extension else {return}
+        guard var path = character.thumbnail?.path else {return}
+        path += ".\(`extension`)"
+        guard let urlPath = URL(string: path) else {return}
+        
+        getImageCharacter(urlPath)
+    }
     
+    func getImageCharacter(_ path: URL) {
+        URLSession.shared.dataTask(with: path ) { data, response, erro in
+            guard
+                let data = data else {return}
+            
+            do {
+                 try data.write(to: self.getDocumentsDirectory().appendingPathComponent("image.jpg"))
+                
+                DispatchQueue.main.async {
+                    self.imageChatacters.image = UIImage(data: data)
+                }
+            } catch {
+                print("Erro\(String(describing: erro))")
+            }
+
+        }.resume()
+    }
     
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
--- Marvel/View/Details/DetailsViewController.swift
+++ Marvel/View/Details/DetailsViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     
+    //MARK: - Atributos
+    let detailView = DetailsViewCode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.red
+        self.view.addSubview(detailView)
+        configConstraints()
+    }
+    
+    func configConstraints() {
+        self.detailView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.view.topAnchor.constraint(equalTo: self.detailView.topAnchor),
+            self.view.leadingAnchor.constraint(equalTo: self.detailView.leadingAnchor),
+            self.view.trailingAnchor.constraint(equalTo: self.detailView.trailingAnchor),
+            self.view.bottomAnchor.constraint(equalTo: self.detailView.bottomAnchor),
+        ])
     }
 }
+
